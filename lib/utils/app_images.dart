6908ca7d914786cdd6587b5b12b51a30aa8f44f4
MuhanadIbrahim <parhoegypt@gmail.com>
2024-06-08 14:55:04 +0300

    create app image file and use flutter asset gent

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,92 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAggressive plan logo
+  /// assets/images/aggressive plan logo.svg
+  static const String imagesAggressiveplanlogo = "assets/images/aggressive plan logo.svg";
+
+  /// Assets for imagesApple buttoun
+  /// assets/images/apple buttoun.svg
+  static const String imagesApplebuttoun = "assets/images/apple buttoun.svg";
+
+  /// Assets for imagesConservative plan logo
+  /// assets/images/conservative plan logo.svg
+  static const String imagesConservativeplanlogo = "assets/images/conservative plan logo.svg";
+
+  /// Assets for imagesCustomized plan logo
+  /// assets/images/customized plan logo.svg
+  static const String imagesCustomizedplanlogo = "assets/images/customized plan logo.svg";
+
+  /// Assets for imagesEgypt flag
+  /// assets/images/egypt flag.svg
+  static const String imagesEgyptflag = "assets/images/egypt flag.svg";
+
+  /// Assets for imagesFaceID
+  /// assets/images/faceID.svg
+  static const String imagesFaceID = "assets/images/faceID.svg";
+
+  /// Assets for imagesForgotPassword logo
+  /// assets/images/forgot-password logo.svg
+  static const String imagesForgotPasswordlogo = "assets/images/forgot-password logo.svg";
+
+  /// Assets for imagesGoogle buttoun
+  /// assets/images/google buttoun.svg
+  static const String imagesGooglebuttoun = "assets/images/google buttoun.svg";
+
+  /// Assets for imagesGreen horizinatil logo
+  /// assets/images/green horizinatil logo.svg
+  static const String imagesGreenhorizinatillogo = "assets/images/green horizinatil logo.svg";
+
+  /// Assets for imagesGreen logo
+  /// assets/images/green logo.svg
+  static const String imagesGreenlogo = "assets/images/green logo.svg";
+
+  /// Assets for imagesGrey horizinail logo
+  /// assets/images/grey horizinail logo.svg
+  static const String imagesGreyhorizinaillogo = "assets/images/grey horizinail logo.svg";
+
+  /// Assets for imagesOnboarding  logo1
+  /// assets/images/onboarding  logo1.svg
+  static const String imagesOnboardinglogo1 = "assets/images/onboarding  logo1.svg";
+
+  /// Assets for imagesOnboarding background1
+  /// assets/images/onboarding background1.svg
+  static const String imagesOnboardingbackground1 = "assets/images/onboarding background1.svg";
+
+  /// Assets for imagesOnboarding background2
+  /// assets/images/onboarding background2.svg
+  static const String imagesOnboardingbackground2 = "assets/images/onboarding background2.svg";
+
+  /// Assets for imagesOnboarding background3
+  /// assets/images/onboarding background3.svg
+  static const String imagesOnboardingbackground3 = "assets/images/onboarding background3.svg";
+
+  /// Assets for imagesOnboarding continue button
+  /// assets/images/onboarding continue button.svg
+  static const String imagesOnboardingcontinuebutton = "assets/images/onboarding continue button.svg";
+
+  /// Assets for imagesOnboarding logo2
+  /// assets/images/onboarding logo2.svg
+  static const String imagesOnboardinglogo2 = "assets/images/onboarding logo2.svg";
+
+  /// Assets for imagesOnboarding logo3
+  /// assets/images/onboarding logo3.svg
+  static const String imagesOnboardinglogo3 = "assets/images/onboarding logo3.svg";
+
+  /// Assets for imagesOnboarding pointer1
+  /// assets/images/onboarding pointer1.svg
+  static const String imagesOnboardingpointer1 = "assets/images/onboarding pointer1.svg";
+
+  /// Assets for imagesOnboarding pointer2
+  /// assets/images/onboarding pointer2.svg
+  static const String imagesOnboardingpointer2 = "assets/images/onboarding pointer2.svg";
+
+  /// Assets for imagesOnboarding pointer3
+  /// assets/images/onboarding pointer3.svg
+  static const String imagesOnboardinpointer3 = "assets/images/onboarding pointer3.svg";
+
+  /// Assets for imagesWhite horizintal logo
+  /// assets/images/white horizintal logo.svg
+  static const String imagesWhitehorizintallogo = "assets/images/white horizintal logo.svg";
+}
+
