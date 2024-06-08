8bf5e50227d7e01aa45cbf401eee73f157829226
MuhanadIbrahim <parhoegypt@gmail.com>
2024-06-08 14:01:59 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/hedg_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hedg_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class AppStyles {
+  static const TextStyle styleRegular12 = TextStyle(
+      color: Color(0xff10284A),
+      fontSize: 12,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w400);
+
+  static const TextStyle styleBold14 = TextStyle(
+      color: Color(0xff10284A),
+      fontSize: 14,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w700);
+
+  static const TextStyle styleMedium12 = TextStyle(
+      color: Color(0xff10284A),
+      fontSize: 12,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500);
+
+  static const TextStyle styleMedium14 = TextStyle(
+      color: Color(0xff10284A),
+      fontSize: 14,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500);
+
+  static const TextStyle styleBold12 = TextStyle(
+      color: Color(0xff10284A),
+      fontSize: 14,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w700);
+
+  static const TextStyle styleMedium8 = TextStyle(
+      color: Color(0xff10284A),
+      fontSize: 8,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500);
+
+  static const TextStyle styleMedium18 = TextStyle(
+      color: Color(0xff10284A),
+      fontSize: 18,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500);
+}
