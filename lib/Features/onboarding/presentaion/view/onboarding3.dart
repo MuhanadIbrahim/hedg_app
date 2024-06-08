cab33deed7117a372800cd0ebba179e512def4be
MuhanadIbrahim <parhoegypt@gmail.com>
2024-06-08 22:49:07 +0300

    almost finish onboarding 3 view

---
+++ lib/Features/onboarding/presentaion/view/onboarding3.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hedg_app/Features/onboarding/presentaion/view/widgets/onboarding3_body_view.dart';
+
+class Onboarding3View extends StatelessWidget {
+  const Onboarding3View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Onboarding3BodyView());
+  }
+}
--- lib/Features/onboarding/presentaion/view/widgets/onboarding3_body_view.dart
+++ lib/Features/onboarding/presentaion/view/widgets/onboarding3_body_view.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:hedg_app/Features/onboarding/presentaion/view/onboarding2.dart';
-import 'package:hedg_app/Features/onboarding/presentaion/view/onboarding3.dart';
 import 'package:hedg_app/core/utils/app_images.dart';
 import 'package:hedg_app/core/utils/app_styles.dart';
 
-class Onboarding2BodyView extends StatelessWidget {
-  const Onboarding2BodyView({super.key});
+class Onboarding3BodyView extends StatelessWidget {
+  const Onboarding3BodyView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +20,7 @@ class Onboarding2BodyView extends StatelessWidget {
           child: Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage(Assets.imagesOnboardingbackground2),
+                image: AssetImage('assets/images/onboarding_background3.png'),
                 fit: BoxFit.contain,
               ),
             ),
@@ -29,42 +29,29 @@ class Onboarding2BodyView extends StatelessWidget {
         Center(
           child: Column(
             children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  SafeArea(
-                      child: Text(
-                    'Skip',
-                    style: AppStyles.styleBold16,
-                  )),
-                  SizedBox(
-                    width: 10,
-                  ),
-                ],
-              ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.13,
               ),
-              SvgPicture.asset(Assets.imagesOnboardinglogo2),
+              SvgPicture.asset(Assets.imagesOnboardinglogo3),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.10,
               ),
               const Text(
-                'Track Your Investments',
+                'Calculate Before Investing',
                 style: AppStyles.styleBold20,
               ),
               const SizedBox(
                 height: 10,
               ),
               Text(
-                'see your return on investments in the \n                      selected plans',
+                'calculate your potential growth to \n choose the perfect option for you',
                 style: AppStyles.styleMedium14
                     .copyWith(color: const Color(0xFF6E838F)),
               ),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.06,
               ),
-              SvgPicture.asset(Assets.imagesOnboardingpointer2),
+              SvgPicture.asset(Assets.imagesOnboardinpointer3),
               SizedBox(
                 height: MediaQuery.of(context).size.height * 0.06,
               ),
@@ -87,7 +74,7 @@ class Onboarding2BodyView extends StatelessWidget {
                   ),
                   GestureDetector(
                     onTap: () {
-                      Get.to(() => const Onboarding3View(),
+                      Get.to(() => const Onboarding2View(),
                           transition: Transition.rightToLeftWithFade,
                           duration: kTabScrollDuration);
                     },
