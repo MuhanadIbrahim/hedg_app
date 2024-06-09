35fe6d4e31d821331049519b9e30361e58173eb6
MuhanadIbrahim <parhoegypt@gmail.com>
2024-06-10 02:41:23 +0300

    create sign up view body

---
+++ lib/Features/sign_up/presentation/view_models/sign_up_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:hedg_app/Features/sign_up/presentation/view_models/widgets/sign_up_view_body.dart';
+
+class SignUpView extends StatelessWidget {
+  const SignUpView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SignUpViewBody());
+  }
+}
+
