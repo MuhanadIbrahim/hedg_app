4378d7c173b4c726234d092e1f49d5dd8bfae4a1
MuhanadIbrahim <parhoegypt@gmail.com>
2024-06-10 23:26:53 +0300

    create national id ve4fication file

---
+++ lib/Features/national_id/presentation/view/national_id_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NationalIdVerficationView extends StatelessWidget {
+  const NationalIdVerficationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+class NationalIdVerficationBodyView extends StatelessWidget {
+  const NationalIdVerficationBodyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
