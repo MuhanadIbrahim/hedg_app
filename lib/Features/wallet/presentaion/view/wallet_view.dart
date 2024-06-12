341e0efd956db69050f5b62bb59a943fd9d52337
MuhanadIbrahim <parhoegypt@gmail.com>
2024-06-12 15:38:27 +0300

    create wallet screan

---
+++ lib/Features/wallet/presentaion/view/wallet_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hedg_app/Features/wallet/presentaion/view/widgets/wallet_body_view.dart';
+
+class WalletView extends StatelessWidget {
+  const WalletView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: WalletBodyView(),
+    );
+  }
+}
+
+
